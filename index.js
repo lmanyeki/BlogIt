0e600153eab4bd23596e53722533109a228bba2f
lmanyeki-debug <lydiahmanyeki@gmail.com>
2025-04-13 21:33:05 +0300

    Authenticates login page

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import cors from 'cors';
-import validateEmailAndUsername from "./validateEmailAndUsername.js";
+import jwt from 'jsonwebtoken';
+import validateEmailAndUsername from "./middleware/validateEmailAndUsername.js";
 import checkPasswordStrength from './middleware/checkPasswordStrength.js';
 // import {pr} from '@prisma/client';
 import pkg from '@prisma/client';
@@ -35,6 +36,39 @@ app.post("/auth/signup", [validateEmailAndUsername, checkPasswordStrength], asyn
         res.status(500).json({ message: "Something went wrong. Please try again." })
     }
 });
+
+app.post("/auth/login", async (req, res) => {
+    try {
+        const { identifier, password } = req.body;
+        const user = await client.user.findFirst({
+            where: {
+                OR: [{ emailAddress: identifier }, { username: identifier }]
+            }
+        });
+        if (!user) {
+            return res.status(401).json({ message: "Wrong login credentials."});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        console.log(isMatch)
+        if (!isMatch) {
+            return res.status(401).json({ message: "Wrong login credentials."});
+        }
+        const jwtPayload = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        }
+        const token = jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY);
+        res.status(200).cookie("blogitAuthToken", token, {}).json({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            emailAddress: user.emailAddress,
+            username: user.username
+        })
+    } catch (e) {
+        res.status(500).json({ message: "Something went wrong."})
+    }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
