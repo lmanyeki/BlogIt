185617ee5a03b6542845129fb728753df3ba257b
lmanyeki-debug <lydiahmanyeki@gmail.com>
2025-04-18 16:08:42 +0300

    Creates profile controller

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -8,7 +8,6 @@ import { fileURLToPath } from 'url';
 const client = new PrismaClient();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const uploadDir = path.join(__dirname, '../uploads');
